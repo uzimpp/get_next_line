60047110f02853c5eb7e0e2668a4ade54d6bd2c0
Worakrit Kullanatpokin <worrakit.boss@gmail.com>
2024-10-09 02:24:33 +0700

    add Tests and updatte main for defense

--- main.c
+++ main.c
@@ -6,35 +6,83 @@
 /*   By: wkullana <wkullana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/06 16:46:24 by wkullana          #+#    #+#             */
-/*   Updated: 2024/10/07 12:08:46 by wkullana         ###   ########.fr       */
+/*   Updated: 2024/10/09 02:23:03 by wkullana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "get_next_line_bonus.h"
-#include "get_next_line.h"
 #include <stdio.h>
 #include <fcntl.h>
+// https://github.com/uzimpp/get_next_line
 
-int	main(int argc, char **argv)
+// ==== Mandatory ====
+// cc -Wall -Wextra -Werror $(ls *.c *.h | grep -v '_bonus')
+// #include "get_next_line.h"
+
+// ==== Bonus ====
+// cc -Wall -Wextra -Werror *_bonus.c *_bonus.h main.c
+#include "get_next_line_bonus.h"
+
+#define NUM_FILES 3
+
+void	test_mandatory(void)
 {
 	int		fd;
 	char	*str;
 	int		i;
+	int		line;
 
-	fd = open("./test", O_RDONLY);
+	fd = open("./test.txt", O_RDONLY);
 	i = 1;
-	str = get_next_line(fd);
-	printf("Time %d: %s", i, str);
-	i++;
+	line = 1;
 	while (1)
 	{
 		str = get_next_line(fd);
 		if (!str)
 			break ;
-		printf("Time %d: %s", i, str);
+		printf("file %d, line %d: %s", i++, line++, str);
 		free(str);
-		i++;
 	}
 	close(fd);
+}
+
+void	test_bonus(void)
+{
+	int		ended[3];
+	char	*str;
+	int		fds[3];
+	int		line;
+	int		i;
+
+	fds[0] = open("tests/test1.txt", O_RDONLY);
+	fds[1] = open("tests/test2.txt", O_RDONLY);
+	fds[2] = open("tests/test3.txt", O_RDONLY);
+	line = 1;
+	for (i = 0; i < 3; i++)
+		ended[i] = 0;
+	while (!(ended[0] && ended[1] && ended[2]))
+	{
+		for (i = 0; i < 3; i++)
+		{
+			str = get_next_line(fds[i]);
+			if (str)
+			{
+				printf("file %d, line %d: %s", i + 1, line++, str);
+				free(str);
+			}
+			else
+			{
+				printf("file %d, line %d: %s\n", i + 1, line++, str);
+				ended[i] = 1;
+			}
+		}
+	}
+	for (i = 0; i < 3; i++)
+		close(fds[i]);
+}
+
+int	main(void)
+{
+	// test_mandatory();
+	test_bonus();
 	return (0);
 }
